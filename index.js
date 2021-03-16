db18d46cfd22fbf3178677669938fdfeca2418f4
Krauzy <30305307+Krauzy@users.noreply.github.com>
2021-03-16 00:30:01 -0300

    doing shit

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: false
+}));
+
+var output = 'Compiled Sucessful!\n\n--runtime: 4.6s'
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {    
-    res.cookie('output', 'Compiled Sucessful!\n\n--runtime: 4.6s');
+    res.cookie('output', output);
     res.render('index.html');
 });
 
+app.post('/compiler', (req, res) => {
+    output = 'Error';
+    console.log(decodeURI(req.body.edit + ''));
+    res.redirect('/');
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
---
+++ src/classes/models/error.js
@@ -0,0 +1,19 @@
+export default class Error {
+    constructor(error, line, column) {
+        this.error = error;
+        this.line = line;
+        this.column = column;
+    }
+
+    getJson() {
+        return {
+            error: this.error,
+            line: this.line,
+            column: this.column
+        };
+    }
+
+    getMessage() {
+        return `ERROR: ${this.error} { line:[${this.line}] column:[${this.column}] }`;
+    }
+}
